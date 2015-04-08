d9225ced2eb2da364878fd0c47dae678adbfc990
Dave Kindel <dkindel@vt.edu>
2015-04-08 17:03:05 -0400

    modified funcs to operate at a single func level

--- kindel_dave_final.c
+++ kindel_dave_final.c
@@ -7,19 +7,42 @@
 #define V_1 1
 #define V_0 0
 
+#define MAXFUNCS 10
+#define MAXCUBES 50
+#define MAXLITERALS 100
 
+//this a single function
+typedef struct SFunc {
+    int numin;
+    int cubes[MAXCUBES][MAXLITERALS];
+    int cubeCount;
+} SFunc;
+
+//This is all of the functions
 typedef struct Func {
     int  numin;
     int  numout;
-    int  cubes[20][100][200]; //first elem is func number, second is the cube, last is the literal
-    //I'm not worried about space so much as simplicity so this works
-    int  cubeCount[20];
+    SFunc  singleFuncs[MAXFUNCS]; //access the individual function
 } Func;
+
+
+typedef struct Kernel {
+    int cubes[MAXCUBES][MAXLITERALS];
+    int cubeCount;
+} Kernel;
+
+typedef struct CoKernel {
+    int cubes[MAXLITERALS];
+} CoKernel;
 
 
 
 Func buildFunc(char*);
 void printFunc(Func* f);
+void rKernel(SFunc *sf, Kernel *k, int kCount);
+void rKernel_allFuncs(Func *f);
+
+
 
 int main(int argc, char *argv[]){
     char espName[20];
@@ -31,6 +54,7 @@ int main(int argc, char *argv[]){
     strcpy(espName, argv[1]);
     Func f = buildFunc(espName);
     printFunc(&f);
+    //RKernel_allFuncs(f);
     return EXIT_SUCCESS;
 }
 
@@ -51,10 +75,11 @@ Func buildFunc(char* espName){
     f.numin = 0;
     f.numout = 0;
     int i;
-    for(i = 0; i < (sizeof(f.cubeCount)/sizeof(int));i++){
-        f.cubeCount[i] = 0; //keeps track of the number of cubes currently in the equation
+    for(i = 0; i < MAXFUNCS; i++){
+        f.singleFuncs[i].cubeCount = 0; //keeps track of the number of cubes currently in the equation
     }
     char line[500]; 
+    //first loop reads in the metadata lines
     while(fgets(line, sizeof line, inFile ) != NULL) {
         if(line[0] == '.'){
             //s is used as a junk array to read in some values
@@ -83,6 +108,7 @@ Func buildFunc(char* espName){
     if(f.numin <= 0 || f.numout <= 0){
         return f;
     }
+    //second loop starts reading in data lines
     while(fgets(line, sizeof line, inFile) != NULL) {
         if(line[0] != '.'){
             char *inputs = malloc(sizeof(char) * f.numin);
@@ -93,7 +119,7 @@ Func buildFunc(char* espName){
             int i; 
             for(i = 0; i < f.numout; i++){
                 if(outputs[i] == '1'){
-                    int currentCube = f.cubeCount[i];
+                    int currentCube = f.singleFuncs[i].cubeCount;
                     int j;
                     for(j = 0; j < f.numin; j++){
                         char inBitChar = inputs[j];
@@ -104,9 +130,9 @@ Func buildFunc(char* espName){
                         else if(inBitChar == '1'){
                             inBit = V_1;
                         }
-                        f.cubes[i][currentCube][j] = inBit;
+                        f.singleFuncs[i].cubes[currentCube][j] = inBit;
                     }
-                    f.cubeCount[i] = f.cubeCount[i] + 1;
+                    f.singleFuncs[i].cubeCount = f.singleFuncs[i].cubeCount + 1;
                 }
             }
             free(inputs);
@@ -121,6 +147,25 @@ Func buildFunc(char* espName){
     return f;
 }
 
+
+//used to call the recursive function on ALL the functions separately (we're going to do
+//this one at a time)
+void rKernel_allFuncs(Func *f){
+    int i;
+    for(i = 0; i < f->numout; i++){
+        Kernel k[100];
+        int kernelCount = 0;
+        SFunc sf = f->singleFuncs[i];
+        sf.numin = f->numin;
+        rKernel(&sf, k, kernelCount);
+    }
+}
+
+//recursive function to find the kernels
+void rKernel(SFunc *sf, Kernel *k, int kCount){
+    
+}
+
 //////////////////
 //helper functions
 //////////////////
@@ -131,13 +176,13 @@ void printFunc(Func* f){
     int i, j, k;
     for(i = 0; i < f-> numout; i++){
         printf("F_%d = ", i);
-        for(j = 0; j < f->cubeCount[i]; j++){
-            for(k = 0; k < (sizeof(f->cubes[i][j]) /sizeof(int)); k++){
-                if(f->cubes[i][j][k] == V_1){
+        for(j = 0; j < f->singleFuncs[i].cubeCount; j++){
+            for(k = 0; k < MAXCUBES; k++){
+                if(f->singleFuncs[i].cubes[j][k] == V_1){
                     printf("%c", intToChar(k));
                 }
             }
-            if(f->cubeCount[i] != j+1){
+            if(f->singleFuncs[i].cubeCount != j+1){
                 printf(" + ");
             }
         }
